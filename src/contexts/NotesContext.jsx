9a03d6fe3c91746b0b5c3c5ef3d059e0da0efa49
JOEL KIGGWE <joelkiggwe94@gmail.com>
2020-11-20 04:37:38 +0300

    Add a Redirects file

--- src/contexts/NotesContext.jsx
+++ src/contexts/NotesContext.jsx
@@ -68,7 +68,8 @@ const reducerFunction = (state, action) => {
       ];
 
     case "DELETE_NOTE":
-      return [];
+       console.log( "Deleteing note before "+ state);
+      return state;
     default:
       return state;
   }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,29 +1,75 @@
 import React, { useContext } from "react";
 import { NotesContext } from "../contexts/NotesContext";
 
-import { Card, CardBody, CardTitle, CardText } from "reactstrap";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardText,
+  UncontrolledCollapse,
+  Button,
+} from "reactstrap";
 
 const HomePage = () => {
-  const { notes } = useContext(NotesContext);
+  const { notes, dispatch } = useContext(NotesContext);
+
+  // Delete functionality
+  const deleteNote = (e) => {
+    console.log("Target Index", e.currentTarget);
+    console.log(
+      "INde",
+      e.currentTarget.parentElement.parentElement.parentElement.querySelector(".note .creation-date").textContent
+    );
+    const removedNoteDate = e.currentTarget.parentElement.parentElement.parentElement.querySelector(".note .creation-date").textContent;
+    const remainingNotes = notes.filter((note) =>new Date(
+      note.createdOn.split("T")[0]
+    ).toLocaleDateString() !== removedNoteDate);
+    console.log(remainingNotes);
+    dispatch({ type: "DELETE_NOTE", notes: remainingNotes });
+    // window.location.reload();
+  };
 
   return (
     <>
-      <main className="container-fluid d-flex flex-row flex-wrap">
+      <main className="container-fluid">
         <h1>Notes</h1>
+        <p className="flex-column">
+          <small className="d-block font-italic">
+            (Click title to expand note!)
+          </small>
+        </p>
         <div className="notes d-flex flex-row flex-wrap">
-          {notes.map((note) => {
+          {notes.map((note, index) => {
             return (
               <>
                 <Card className="note">
-                  <CardBody>
-                    <CardTitle className="note-title" tag="h5">
-                      {note.subject}
-                    </CardTitle>
-                    <CardText className="note-category">
-                      {note.category}
-                    </CardText>
-                    <small>{new Date(note.createdOn.split("T")[0]).toLocaleDateString()}</small>
-                  </CardBody>
+                  <CardTitle
+                    className="note-title"
+                    id={"toggler" + ++index}
+                    tag="h3"
+                  >
+                    {note.subject}
+                  </CardTitle>
+                  <p>{note.category}</p>
+                  <small className="creation-date"> 
+                    {new Date(
+                      note.createdOn.split("T")[0]
+                    ).toLocaleDateString()}
+                  </small>
+                  <UncontrolledCollapse toggler={"#toggler" + index}>
+                    <CardBody>
+                      <CardText>{note.about}</CardText>
+                    </CardBody>
+                    <div className="d-flex justify-content-between">
+                      <Button
+                        onClick={(e, index) => deleteNote(e, index)}
+                        className="btn btn-danger pl-3 pr-3"
+                      >
+                        Delete
+                      </Button>
+                      {/* <Button onClick={} className="btn btn-info pl-4 pr-4">Edit</Button> */}
+                    </div>
+                  </UncontrolledCollapse>
                 </Card>
               </>
             );
